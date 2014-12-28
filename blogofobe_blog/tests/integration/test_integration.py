87213fde460b50147e00c69123cd62377c4ccdaa
Walter Lapchynski <wxl@ubuntu.com>
2014-12-28 15:37:15 -0800

    blogofobed

--- blogofobe_blog/tests/integration/test_integration.py
+++ blogofobe_blog/tests/integration/test_integration.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Integration tests for blogofile_blog plugin.
+"""Integration tests for blogofobe_blog plugin.
 """
 import os
 import shutil
@@ -8,22 +8,22 @@ try:
     import unittest2 as unittest        # For Python 2.6
 except ImportError:
     import unittest                     # flake8 ignore # NOQA
-from blogofile import main
+from blogofobe import main
 
 
-class TestBlogofileBlogCommands(unittest.TestCase):
-    """Intrgration tests for the blogofile_blog commands.
+class TestblogofobeBlogCommands(unittest.TestCase):
+    """Intrgration tests for the blogofobe_blog commands.
     """
     def _call_entry_point(self, *args):
         main.main(*args)
 
-    def test_blogofile_init_blog_site(self):
-        """`blogofile init src blog` initializes blog site w/ expected files
+    def test_blogofobe_init_blog_site(self):
+        """`blogofobe init src blog` initializes blog site w/ expected files
         """
         src_dir = mkdtemp()
         self.addCleanup(shutil.rmtree, src_dir)
         os.rmdir(src_dir)
-        self._call_entry_point(['blogofile', 'init', src_dir, 'blog'])
+        self._call_entry_point(['blogofobe', 'init', src_dir, 'blog'])
         self.assertEqual(
             set(os.listdir(src_dir)),
             set('_config.py 404.html _htaccess _posts _templates '
@@ -31,13 +31,13 @@ class TestBlogofileBlogCommands(unittest.TestCase):
                 'robots.txt themes'
                 .split()))
 
-    def test_blogofile_build_blog_site(self):
-        """`blogofile build` on blog site creates _site directory
+    def test_blogofobe_build_blog_site(self):
+        """`blogofobe build` on blog site creates _site directory
         """
         self.addCleanup(os.chdir, os.getcwd())
         src_dir = mkdtemp()
         self.addCleanup(shutil.rmtree, src_dir)
         os.rmdir(src_dir)
-        self._call_entry_point(['blogofile', 'init', src_dir, 'blog'])
-        self._call_entry_point(['blogofile', 'build', '-s', src_dir])
+        self._call_entry_point(['blogofobe', 'init', src_dir, 'blog'])
+        self._call_entry_point(['blogofobe', 'build', '-s', src_dir])
         self.assertIn('_site', os.listdir(src_dir))
